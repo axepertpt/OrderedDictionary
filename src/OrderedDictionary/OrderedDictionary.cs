f262f1e4e40009313eebb855a0ea6b29b9e2c9dc
p.topor <p.topor@dextechnology.com>
2023-06-07 10:42:01 +0300

    Реализовал OrderedDictionary

---
+++ src/OrderedDictionary/OrderedDictionary.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+
+namespace OrderedDictionary;
+
+public class OrderedDictionary<TKey, TValue> : IDictionary<TKey, TValue> where TKey : notnull
+{
+    private readonly LinkedList<KeyValuePair<TKey, TValue>> _linkedList = new();
+    private readonly Dictionary<TKey, TValue> _dictionary = new();
+
+    public TValue this[TKey key]
+    {
+        get => _dictionary[key];
+        set => _dictionary[key] = value;
+    }
+
+    public ICollection<TKey> Keys => _dictionary.Keys;
+    public ICollection<TValue> Values => _dictionary.Values;
+    public int Count => _dictionary.Count;
+    public bool IsReadOnly => false;
+
+    public void Add(TKey key, TValue value)
+    {
+        if (_dictionary.ContainsKey(key) is false)
+        {
+            _dictionary.Add(key, value);
+            _linkedList.AddLast(new KeyValuePair<TKey, TValue>(key, value));
+        }
+        else
+        {
+            throw new ArgumentException("An item with the same key has already been added.");
+        }
+    }
+
+    public bool ContainsKey(TKey key)
+    {
+        return _dictionary.ContainsKey(key);
+    }
+
+    public bool Remove(TKey key)
+    {
+        if (_dictionary.TryGetValue(key, out var value) is false) 
+            return false;
+
+        _dictionary.Remove(key);
+        _linkedList.Remove(new KeyValuePair<TKey, TValue>(key, value));
+        return true;
+    }
+
+    public bool TryGetValue(TKey key, out TValue value)
+    {
+        return _dictionary.TryGetValue(key, out value);
+    }
+
+    public void Add(KeyValuePair<TKey, TValue> item)
+    {
+        Add(item.Key, item.Value);
+    }
+
+    public void Clear()
+    {
+        _dictionary.Clear();
+        _linkedList.Clear();
+    }
+
+    public bool Contains(KeyValuePair<TKey, TValue> item)
+    {
+        return _dictionary.Contains(item);
+    }
+
+    public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+    {
+        _linkedList.CopyTo(array, arrayIndex);
+    }
+
+    public bool Remove(KeyValuePair<TKey, TValue> item)
+    {
+        return Remove(item.Key);
+    }
+
+    public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
+    {
+        return _linkedList.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
---
+++ src/TestOrderedDictionary/Program.cs
@@ -0,0 +1,19 @@
+﻿using OrderedDictionary;
+
+var orderedDictionary = new OrderedDictionary<int, string>();
+
+for (var i = 0; i<100; i++)
+{
+    var random = new Random();
+    while (orderedDictionary.TryAdd(random.Next(101), $"Вставка по порядку #{i}") is false)
+    {
+
+    }
+}
+
+foreach (var item in orderedDictionary)
+{
+    Console.WriteLine(item.Key+"\t"+item.Value);
+}
+
+Console.ReadKey();
